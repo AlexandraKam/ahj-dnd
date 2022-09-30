b1357453e0842cfecdb8387fdac25b41bfced804
Александра Каменева <70579446+AlexandraKam@users.noreply.github.com>
2022-09-30 14:48:00 +0500

    Update Logic.js

--- src/js/Logic.js
+++ src/js/Logic.js
@@ -1,62 +1,117 @@
-export default class Gui {
-  constructor() {
-    this.btn = document.querySelector('.btn');
-    this.itemsAll = document.querySelectorAll('.items');
-    this.cols = document.querySelectorAll('.column');
-    this.input = document.querySelector('.input');
-    this.close = document.querySelector('.close');
-    this.taskAll = document.querySelectorAll('.items-item');
-    this.container = document.querySelector('.container');
-    this.select = document.querySelector('.select');
+import { getElementForInsert } from './function';
+
+export default class Logic {
+  constructor(gui) {
+    this.gui = gui;
+    this.addTask = this.addTask.bind(this);
+    this.addEvent = this.addEvent.bind(this);
+    this.moveTask = this.moveTask.bind(this);
   }
 
-  createTask(value, status) {
-    const task = document.createElement('li');
-    const closeBtn = document.createElement('span');
-    closeBtn.classList.add('close');
-    closeBtn.classList.add('hidden');
-    task.classList.add('items-item');
-    task.textContent = value;
-    task.append(closeBtn);
-    this.cols.forEach((el) => {
-      const header = el.querySelector('.header');
-
-      if (header.textContent === status) {
-        el.querySelector('.items').append(task);
-      }
+  init() {
+    this.gui.btn.addEventListener('click', (e) => this.addTask(e));
+    this.moveTask(this.gui.container);
+    this.gui.tasksListFromLS();
+    const taskNewAll = document.querySelectorAll('.items-item');
+    taskNewAll.forEach((el) => {
+      this.addEvent(el);
     });
-    return task;
   }
 
-  getTasksListObjFromDom() {
-    const tasksList = {};
-    this.itemsAll.forEach((ul) => {
-      const lis = [...ul.children];
-      const h3 = ul.closest('.column').children[0].innerText;
-      tasksList[h3] = [];
-      lis.forEach((li) => {
-        tasksList[h3].push(li.innerText);
-      });
-    });
-    return tasksList;
-  }
-
-  tasksListFromLS() {
-    if (!localStorage.dragNdrops) return;
-    const tasksList = JSON.parse(localStorage.dragNdrops);
-
-    // eslint-disable-next-line
-    for (const title in tasksList) {
-      if (Object.prototype.hasOwnProperty.call(tasksList, title)) {
-        this.cols.forEach((col) => {
-          if (col.children[0].innerText === title) {
-            const ul = col.children[1];
-            tasksList[title].forEach((li) => {
-              ul.append(this.createTask(li, title));
-            });
-          }
-        });
-      }
+  addTask(e) {
+    e.preventDefault();
+    if (this.gui.input.classList.contains('hidden')) {
+      this.gui.input.classList.remove('hidden');
+      this.gui.select.classList.remove('hidden');
+    } else if (this.gui.input.value.length === 0) {
+      // eslint-disable-next-line no-alert
+      alert('Please, write title task');
+    } else if (this.gui.select.value.length === 0) {
+      // eslint-disable-next-line no-alert
+      alert('Please, write status task');
+    } else {
+      this.el = this.gui.createTask(this.gui.input.value, this.gui.select.value);
+      this.gui.input.value = '';
+      this.gui.select.value = '';
+      this.addEvent(this.el);
+      this.gui.input.classList.add('hidden');
+      this.gui.select.classList.add('hidden');
+      localStorage.dragNdrops = JSON.stringify(this.gui.getTasksListObjFromDom());
     }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addEvent(el) {
+    const closeBtn = el.querySelector('.close');
+    el.addEventListener('mouseover', () => {
+      closeBtn.classList.remove('hidden');
+    });
+    el.addEventListener('mouseout', () => {
+      closeBtn.classList.add('hidden');
+    });
+    closeBtn.addEventListener('click', () => {
+      closeBtn.parentElement.remove();
+      localStorage.dragNdrops = JSON.stringify(this.gui.getTasksListObjFromDom());
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  moveTask(itemsEl) {
+    let draggedEl = null;
+    let ghostEl = null;
+    let shiftX = null;
+    let shiftY = null;
+    let insertOption = null;
+    const insertElem = document.createElement('li');
+    insertElem.classList.add('place');
+    insertElem.classList.add('items-item');
+
+    itemsEl.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      if (!e.target.classList.contains('items-item')) {
+        return;
+      }
+      shiftX = e.clientX - e.target.getBoundingClientRect().left;
+      shiftY = e.clientY - e.target.getBoundingClientRect().top;
+      draggedEl = e.target;
+      insertOption = getElementForInsert(e.clientX, e.clientY);
+      ghostEl = e.target.cloneNode(true);
+      ghostEl.classList.add('dragged');
+      document.body.appendChild(ghostEl);
+      ghostEl.style.left = `${e.pageX - shiftX}px`;
+      ghostEl.style.top = `${e.pageY - shiftY}px`;
+    });
+
+    itemsEl.addEventListener('mousemove', (e) => {
+      e.preventDefault();
+      if (!draggedEl) {
+        return;
+      }
+      insertOption = getElementForInsert(e.clientX, e.clientY);
+      insertOption.parent.insertBefore(insertElem, insertOption.child);
+      ghostEl.style.left = `${e.pageX - shiftX}px`;
+      ghostEl.style.top = `${e.pageY - shiftY}px`;
+    });
+    itemsEl.addEventListener('mouseleave', () => {
+      if (!draggedEl) {
+        return;
+      }
+      document.body.removeChild(ghostEl);
+      ghostEl = null;
+      draggedEl = null;
+    });
+    itemsEl.addEventListener('mouseup', () => {
+      if (!draggedEl) {
+        return;
+      }
+      insertOption.parent.insertBefore(draggedEl, insertOption.child);
+      if (document.querySelector('.place')) {
+        insertOption.parent.removeChild(insertElem);
+      }
+      document.body.removeChild(ghostEl);
+      localStorage.dragNdrops = JSON.stringify(this.gui.getTasksListObjFromDom());
+      ghostEl = null;
+      draggedEl = null;
+    });
   }
 }
